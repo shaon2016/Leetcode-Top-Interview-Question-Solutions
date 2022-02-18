2fed0a71e9ec131e8e86541039729f0aca2db7c3
Md Ashiqul Islam <shaondeveloper@gmail.com>
2022-02-18 20:00:16 +0600

    zalando task 2 previous solution optimized

---
+++ zalando/Task2.kt
@@ -0,0 +1,53 @@
+import kotlin.math.abs
+
+fun main() {
+    println(solution1(-999))
+}
+
+
+fun solution1(N: Int): Int {
+    val digit = 5
+    if (N == 0) return digit * 10
+
+    var isNegative = false
+
+    // Checking this number is negative or not
+    if (N < 0) isNegative = true
+
+    // Making the number no negative
+    val str = abs(N).toString()
+
+    var newStr = ""
+    var i = 0
+
+    while (i < str.length) {
+        val currentNum: Int = str[i] - '0'
+
+        if (isNegative) {
+            if (currentNum > digit) {
+                newStr += digit
+                break
+            }
+        } else {
+            if (currentNum < digit) {
+                newStr += digit
+                break
+            }
+        }
+        newStr += currentNum
+
+        i++
+    }
+
+    // placing the rest of the number
+    while (i < str.length) {
+        val currentNum: Int = str[i++] - '0'
+        newStr += currentNum
+    }
+
+
+    var newNumber = newStr.toInt()
+    if (isNegative) newNumber *= -1
+
+    return newNumber
+}
