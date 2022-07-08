681a00e7b320c935e814d4d90c8cf11fc804c423
Md Ashiqul Islam <shaondeveloper@gmail.com>
2022-07-08 23:45:25 +0600

    BFS: bfs implemented

--- data_structure/graph/BFS.kt
+++ data_structure/graph/BFS.kt
@@ -4,12 +4,29 @@ import java.util.*
 
 data class Vertex(
     val data: Int,
-    val visited: Boolean = false,
+    var visited: Boolean = false,
     var neighbours: List<Vertex> = LinkedList(),
 )
 
 class BFS(private val startVertex: Vertex) {
+    fun traverse() {
+        val queue: Queue<Vertex> = LinkedList()
+        queue.add(startVertex)
 
+        while (queue.isNotEmpty()) {
+            // remove and poll method remove an item from queue
+            // But remove throws exception if queue empty
+            // poll return null if q empty
+            val currentVertex = queue.remove()
+
+            if (!currentVertex.visited) {
+                currentVertex.visited = true
+                println(currentVertex.data)
+                queue.addAll(currentVertex.neighbours)
+            }
+        }
+
+    }
 }
 
 fun main() {
@@ -22,4 +39,6 @@ fun main() {
     v1.neighbours = LinkedList(arrayListOf(v2))
     v2.neighbours = LinkedList(arrayListOf(v0, v3))
     v3.neighbours = LinkedList(arrayListOf(v3))
+
+    BFS(v2).traverse()
 }
